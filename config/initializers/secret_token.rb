6c49cc74b315477ae156dec270aadfd778082947
Stuart Ellidge <stuellidge@Paddington.local>
2013-01-03 21:06:55 +0000

    Modified to prevent SQL injection attacks

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FoodBank::Application.config.secret_token = 'a86526e16caef4bb13ed271a6c6d09c5d6a52c66f884cd9c332f70df485bd9e5bc3bc493e2c8137b1818dbfbd066304fef8a2dac99eca7531e2435278c259161'
+
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+  raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+FoodBank::Application.config.secret_token =
+  ENV['SECRET_TOKEN'] || 'a86526e16caef4bb13ed271a6c6d09c5d6a52c66f884cd9c332f70df485bd9e5bc3bc493e2c8137b1818dbfbd066304fef8a2dac99eca7531e2435278c259161'
