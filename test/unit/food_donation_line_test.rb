d11f8cd6b67c3b63351379f49357994490e2cfbc
Stuart Ellidge <stuellidge@Paddington.local>
2013-03-17 16:17:10 +0000

    Removing associated files

--- test/unit/food_donation_line_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FoodDonationLineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/food_donation_lines_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FoodDonationLinesHelperTest < ActionView::TestCase
-end
